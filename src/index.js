5372cc9ad10fbd5e1b3977ed2573f4094e2e83a7
vzanimonets <vzanimonets@gmail.com>
2019-03-24 20:36:13 +0300

    feat: getZerosCount function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-}
+  let ZerosCount = [];
+  let b = base;
+  const n = number;
+
+  for (let i = 2; i <= b; i++) {
+    if (b % i === 0) {
+      let dividerCount = 0;
+      while (b % i === 0) {
+        b /= i;
+        dividerCount++;
+      }
+      let count = 0;
+      let z = Math.floor(n / i);
+      while (z > 0) {
+        count += z;
+        z = Math.floor(z / i);
+      }
+      ZerosCount.push(Math.floor(count / dividerCount));
+    }
+  }
+  return Math.min(...ZerosCount);
+};
